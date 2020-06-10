beee858deb5bd0c7cede1587385312b645fba6f0
Diar <qoroviqidiar@gmail.com>
2020-06-10 19:30:36 +0200

    Create routes and use model

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    trype: String,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
